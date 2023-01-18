32ea0c10f396d2218b44fa46b3a829091940c95b
Md.Rakib Hosen <37983022+rakibthpi@users.noreply.github.com>
2023-01-19 02:17:24 +0600

    email send successful firebase authentication

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,27 +1,42 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 const register = {
     boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
     padding: "20px",
     borderRadius: "10px",
 }
-const auth = getAuth();
 
 
-// Funcrtion  start 
-const handleEmail = (event) => {
-    console.log(event.target.value)
-}
-const handlePassword = e => {
-    console.log(e.target.value)
-}
-const handleSubmit = event => {
-    event.preventDefault()
-    console.log("Form Submit");
-}
+
+
 const Forms = () => {
+    const auth = getAuth();
+
+    const [emailuser, setEmailuser] = useState('');
+    const [passuser, setPassuser] = useState('');
+
+
+    // Funcrtion  start 
+    const handleEmail = (event) => {
+        setEmailuser(event.target.value)
+    }
+    const handlePassword = e => {
+        setPassuser(e.target.value)
+    }
+    const handleSubmit = event => {
+        event.preventDefault()
+        console.log("Form Submit", "Email", emailuser, "password", passuser);
+        createUserWithEmailAndPassword(auth, emailuser, passuser)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     return (
         <div>
             <div style={register} className=' w-25 mx-auto'>
